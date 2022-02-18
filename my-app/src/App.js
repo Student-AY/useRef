48fcd440871079658c7f1236d8058d86f57f4347
Student-AY <Student-Ahmet-Yilmaz@hotmail.com>
2022-02-18 14:04:53 +0100

    klaar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import {useRef , useState , useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [name , setName] = useState('');
+  const prevName = useRef('');
+
+  useEffect(() => {
+    prevName.current = name;
+  },[name])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <input value={name} onChange={e => setName(e.target.value)}/>
+    <div>my name is {name} but it used to be {prevName.current}</div>
+    
+    </>
   );
 }
 
